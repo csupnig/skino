ba3c86000128fa579075e289fffe82259c1051c2
Christopher Supnig <christopher@supnig.com>
2022-10-24 19:04:01 +0200

    make slider faster

--- assets/custom-slider.js
+++ assets/custom-slider.js
@@ -2,11 +2,10 @@ $(document).ready(function(){
     var $slider = $('#only-slider');
     $slider.slick({
         arrows: false,
-        slidesToShow: 5,
+        slidesToShow: Math.floor($('.collection-list-wrapper').width() / 200),
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
     });
-    console.log('JQ', $slider.slick, $slider, Math.floor($('.collection-list-wrapper').width() / 200));
     $('.slider-mobile-gutter').show();
 });
